f306212226b9f089db43f84f3ef497c1c310bced
RydertHuGlIfE <varunchauhan2001dma@gmail.com>
2026-03-10 10:29:26 +0530

    updated install script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-echo "Setting up RSTUI dependencies..."
+echo "Setting up VELLTUI dependencies..."
 
 if [ -f /etc/arch-release ]; then
     sudo pacman -S --needed rsync sshpass python-colorama
+    sudo pacman -S --needed btop 
 elif [ -f /etc/debian_version ]; then
     sudo apt update
     sudo apt install -y rsync sshpass python3-colorama
+    sudo apt install btop
+
+elif [-f /etc/fedora_version]; then 
+    sudo dnf install -y rsync sshpass python3-colorama
+    sudo dnf install btop 
 else
     echo "Unsupported OS. Please install rsync, sshpass and colorama manually."
     exit 1

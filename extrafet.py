57c6beffcc278411eb04267a6c4d94556c1af7bd
RydertHuGlIfE <varunchauhan2001dma@gmail.com>
2026-03-11 10:34:25 +0530

    somethign maybe?

--- extrafet.py
+++ extrafet.py
@@ -14,4 +14,22 @@ def system_monitor(user, host, password):
 def remote_shell(user, host, password):
     shell_cmd = f"sshpass -p '{password}' ssh -tt -o StrictHostKeyChecking=no {user}@{host} zsh"
     subprocess.run(shell_cmd, shell=True)
+
+
+def server_control(user, host, password):
+    print("\n Server Control Menu... ")
+    print("1: Reboot Server")
+    print("2: Shutdown Server")
+    print("3: Exit")
+    choice = input("Enter your choice: [1/2/3]: ")
+
+    if choice == "1":
+        reboot_cmd = f"sshpass -p '{password}' ssh -tt -o StrictHostKeyChecking=no {user}@{host} sudo reboot"
+        subprocess.run(reboot_cmd, shell=True)
+    if choice == "2":
+        shutdown_cmd = f"sshpass -p '{password}' ssh -tt -o StrictHostKeyChecking=no {user}@{host} sudo shutdown now"
+        subprocess.run(shutdown_cmd, shell=True)
+    if choice == "3":
+        return
+        
     
